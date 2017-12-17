94d096198ee3e2c59ca2b2d9a24f0a96700c18b7
darrellneilcouch <darrellneilcouch@gmail.com>
2017-12-17 12:50:28 -0700

    edited seed errors

--- db/seeds/02_partners.js
+++ db/seeds/02_partners.js
@@ -7,7 +7,7 @@ exports.seed = function(knex, Promise) {
       return knex('partners').insert([
         {first_name: 'Chris', middle_initial: 'M', last_name: 'Skelly', title: "Hon.", email: 'cskelly@smoadr.com', img: "http://www.nadn.org/photos/Chris-Skelly.jpg"},
         {first_name: 'Charles', middle_initial: 'J', last_name: 'Muchmore', title: "", email: 'cmuchmore@smoadr.com', img: "http://www.nadn.org/photos/Charles-Muchmore.jpg"},
-        {firsrt_name: 'Robert', midde_initial: 'H', last_name: 'Oberbillig', title: 'Hon.', email: 'roberbillig@smoadr.com', imp: "http://www.nadn.org/photos/Steve-Scott.jpg"}
+        {first_name: 'Robert', midde_initial: 'H', last_name: 'Oberbillig', title: 'Hon.', email: 'roberbillig@smoadr.com', imp: "http://www.nadn.org/photos/Steve-Scott.jpg "}
       ]);
     });
 };
