3c4b4aadcd624ea86192f71902303c578f403141
darrellneilcouch <darrellneilcouch@gmail.com>
2017-12-17 12:42:35 -0700

    added Robert Oberbilig

--- db/seeds/02_partners.js
+++ db/seeds/02_partners.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('partners').insert([
-        {first_name: 'Stephen', middle_initial: 'H', last_name: 'Scott', title: "Hon.", email: 'sscott@ssmadr.com', img: "http://www.nadn.org/photos/Steve-Scott.jpg"},
-        {first_name: 'Chris', middle_initial: 'M', last_name: 'Skelly', title: "Hon.", email: 'cskelly@ssmadr.com', img: "http://www.nadn.org/photos/Chris-Skelly.jpg"},
-        {first_name: 'Charles', middle_initial: 'J', last_name: 'Muchmore', title: "", email: 'cmuchmore@ssmadr.com', img: "http://www.nadn.org/photos/Charles-Muchmore.jpg"},
+        {first_name: 'Chris', middle_initial: 'M', last_name: 'Skelly', title: "Hon.", email: 'cskelly@smoadr.com', img: "http://www.nadn.org/photos/Chris-Skelly.jpg"},
+        {first_name: 'Charles', middle_initial: 'J', last_name: 'Muchmore', title: "", email: 'cmuchmore@smoadr.com', img: "http://www.nadn.org/photos/Charles-Muchmore.jpg"},
+        {firsrt_name: 'Robert', midde_initial: 'H', last_name: 'Oberbillig', title: 'Hon.', email: 'roberbillig@smoadr.com', imp: "http://www.nadn.org/photos/Steve-Scott.jpg"}
       ]);
     });
 };
